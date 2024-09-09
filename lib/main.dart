9c6ccf33276e43b89423b0eb9c1a2b84c901d95b
Levent Kantaroğlu <63751824+leventkantaroglu@users.noreply.github.com>
2024-09-10 00:44:27 +0300

    Add and apply  flutter_localizations

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_localizations/flutter_localizations.dart';
+
 void main() {
   runApp(const MainApp());
 }
@@ -10,9 +12,38 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('en', 'US'),
+        Locale('tr', 'TR'),
+        // Locale ('ar', ''),
+      ],
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ListTile(
+              leading: const Icon(Icons.check),
+              title: Text(
+                MaterialLocalizations.of(context).okButtonLabel,
+              ),
+            )
+          ],
         ),
       ),
     );
